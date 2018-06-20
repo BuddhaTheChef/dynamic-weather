6c7d7674f45ee85d9c442074128feaef4439c090
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-06-19 14:26:35 -1000

    setup for dynamic map

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import SearchBar from '../containers/search_bar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        React Simple Starter 
+        <SearchBar />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/containers/search_bar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    }
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({term: event.target.value})
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          placeholder="Get a 5 day forecast in your favorite cities"
+          className=""
+          value={this.state.term}
+          onChange={this.onInputChange}
+         />
+        <span>
+          <button type="submit" className="">Submit</button>
+        </span>
+      </form>
+    )
+  }
+}
+
+export default SearchBar
