cb549b555db71873294bfd94c07981f69285d811
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-06-21 14:03:16 -1000

    displayed data from api and added components

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import SearchBar from '../containers/search_bar';
+import WeatherList from '../containers/weather_list';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        React Simple Starter
+        Dynamic Weather
         <SearchBar />
+        <WeatherList />
       </div>
     )
   }
