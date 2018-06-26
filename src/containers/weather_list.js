34ad5bced015f2ea33ed1a82ef0c1091bc9b790d
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-06-26 11:06:14 -1000

    added pressure and humidity also converted kelvin into farenheit

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import _ from 'lodash';
 
 class WeatherList extends Component {
 
   renderWeather(cityData) {
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp * 9/5 -459.67);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humiditys = cityData.list.map(weather => weather.main.humidity);
     console.log(temps);
 
     return(
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color="green" />
-        </td>
+        <td><Chart data={temps} color="green" units="&#8457;" /></td>
+        <td><Chart data={pressures} color="black" units="hPa" /></td>
+        <td><Chart data={humiditys} color="blue" units="%" /></td>
       </tr>
     );
   }
@@ -25,9 +28,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (&#8457;)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
