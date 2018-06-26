d4bb053e55cfeb662a22ec4f0f15f4098d33056c
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-06-26 10:40:52 -1000

    refactoring code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 /////////////INSERT API KEY HERE //////////////////
-const API_KEY = '';
+const API_KEY = '3fa3f989c39659651f5aef4d25b43421';
 /////////////INSERT API KEY HERE //////////////////
 
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
